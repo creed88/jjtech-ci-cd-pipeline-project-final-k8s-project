58ab86f28d8621dcc10ca8fbc4897d74ffbc8a74
creed88 <chrlschukwudi@yahoo.com>
2024-10-02 17:18:52 -0400

    updated sonarqube and nexus ip in codebase

--- terraform/eks-cluster-ec2/provider.tf
+++ terraform/eks-cluster-ec2/provider.tf
@@ -3,12 +3,12 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "prosperous-bucket-2024"
+    bucket         = "final-k8s-prj-creed88"
     key            = "jjtech/terraform.tfstate"
     region         = "us-east-1"
 
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "prosperous-db"
+    dynamodb_table = "final-k8s-table"
   }
   
   required_providers {
